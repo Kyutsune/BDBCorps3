03fb44cda6674ebc81cda711b7c342120e37f1ec
Ferîşte-Beep-Boop <angel.munoz692003@gmail.com>
2024-02-13 15:51:00 +0100

    Création du tableau d'alliée et d'ennemis
    Tout les alliées et les ennemis sont dans un tableau distincts
    Il faut désormais que les alliées trouvent l'ennemie le plus proche et vice versa, car pour le moment on va vers l'ennemie[0].
    Attention cela à rajouter des bugs surement liées aux déplacements:
    - ArgumentOutOfRangeException: Index was out of range. Must be non-negative and less than the size of the collection.
    Parameter name: index
    System.Collections.Generic.List`1[T].get_Item (System.Int32 index) (at <bfbc89a04b6b4dd49c3614d03fc31ce8>:0)
    Jeu.Update () (at Assets/Script/Jeu.cs:90)
    
    - des Unites se crée en trop

--- Projet_unity/Assets/Script/Jeu.cs
+++ Projet_unity/Assets/Script/Jeu.cs
@@ -9,9 +9,12 @@ Classe jeu qui va être celle qui va contenir notre boucle de jeu,essayons de co
 public class Jeu : MonoBehaviour
 {
     private List<GameObject> unites = new List<GameObject>();
+    private List<Unite> alliee = new List<Unite>();
+    private List<Unite> ennemie = new List<Unite>();
     private Unite unite1;
     private Unite unite2;
-    private int nb_unite;
+    private int nb_alliee;
+    private int nb_ennemie;
     public GameObject prefab;
     Animator animator;
 
@@ -28,71 +31,89 @@ public class Jeu : MonoBehaviour
         canva_pour_texte_pv.renderMode = RenderMode.ScreenSpaceOverlay;
         
 
-        unite1 = new Unite(0, 0, 0, 1000, 2, 1,Team.Equipe1, Type_unitee.Melee, canva_pour_texte_pv);
-        unite2 = new Unite(10, 0, 0, 1000, 1, 2,Team.Equipe2, Type_unitee.Melee, canva_pour_texte_pv);
-        nb_unite=2;
+        //unite1 = new Unite(0, 0, 0, 1000, 2, 1,Team.Equipe1, Type_unitee.Melee, canva_pour_texte_pv);
+        nb_alliee=1;
+        nb_ennemie=1;
 
         
 
         animator = GetComponent<Animator>();
 
 
-        //Ici on va créer le texte des pvs pour chaque unité
-        for(int i=0;i<nb_unite;i++)
+        for(int i=0;i<nb_alliee+nb_ennemie;i++) // Parcours du nombre d'ennemis + alliées
         {
+            for(int j=0;j<nb_alliee;j++) //Parcours du nombre d'alliées
+            {
+                alliee.Add(new Unite(0, 0, 0, 1000, 2, 1, Team.Equipe1, Type_unitee.Melee, canva_pour_texte_pv));
 
-            float positionX_texte_pv = unite1.PositionX; // Exemple de position X (vous pouvez ajuster selon vos besoins)
-            float positionY_texte_pv = unite1.PositionY+40; // Exemple de position Y (vous pouvez ajuster selon vos besoins)
-            float positionZ_texte_pv = unite1.PositionZ; // Exemple de position Z (vous pouvez ajuster selon vos besoins)
-        
-            // Créer le texte PV et l'ajouter à la liste
-            AffichageDesPVs textePVUnite=GetComponent<AffichageDesPVs>();
-            textePVUnite.CreerTextePV(canva_pour_texte_pv, positionX_texte_pv, positionY_texte_pv, positionZ_texte_pv);
-            liste_texte_pv.Add(textePVUnite);
+                float positionX_texte_pv = alliee[j].PositionX; // Exemple de position X (vous pouvez ajuster selon vos besoins)
+                float positionY_texte_pv = alliee[j].PositionY+40; // Exemple de position Y (vous pouvez ajuster selon vos besoins)
+                float positionZ_texte_pv = alliee[j].PositionZ; // Exemple de position Z (vous pouvez ajuster selon vos besoins)
+            
+                // Créer le texte PV et l'ajouter à la liste
+                AffichageDesPVs textePVUnite=GetComponent<AffichageDesPVs>();
+                textePVUnite.CreerTextePV(canva_pour_texte_pv, positionX_texte_pv, positionY_texte_pv, positionZ_texte_pv);
+                liste_texte_pv.Add(textePVUnite);
+
+                CreerUnite(alliee[j].PositionX, alliee[j].PositionY, alliee[j].PositionZ);
+                
+            } 
+            for(int t=0;t<nb_alliee;t++) //parcours du nombre d'ennemies
+            {
+                ennemie.Add(new Unite(10, 0, 0, 1000, 1, 2,Team.Equipe2, Type_unitee.Melee, canva_pour_texte_pv));
+                CreerUnite(ennemie[t].PositionX, ennemie[t].PositionY, ennemie[t].PositionZ);
+            }     
+            
         }
         
 
-        CreerUnite(unite1.PositionX, unite1.PositionY, unite1.PositionZ);
-        CreerUnite(unite2.PositionX, unite2.PositionY, unite2.PositionZ);
+        
     }
 
     void Update()
     {
-        if (unite1 != null && unite2 != null && unites != null && unites.Count >= 2)
+        
+        for(int i=0;i<nb_alliee+nb_ennemie;i++) // Parcours du nombre d'ennemis + alliées
         {
-            unite1.DeplacerVersUniteDifferente(unite2, canva_pour_texte_pv);
-            unite2.DeplacerVersUniteDifferente(unite1, canva_pour_texte_pv);
-           for(int i=0;i<nb_unite;i++)
+
+            for(int j=0;j<nb_alliee;j++) //Parcours du nombre d'alliées
             {
-                
-                float positionX_texte = unite1.PositionX ; // Exemple de position X (vous pouvez ajuster selon vos besoins)
-                float positionY_texte = unite1.PositionY+40; // Exemple de position Y (vous pouvez ajuster selon vos besoins)
+                alliee[j].DeplacerVersUniteDifferente(ennemie[0], canva_pour_texte_pv);
+                float positionX_texte = alliee[j].PositionX ; // Exemple de position X (vous pouvez ajuster selon vos besoins)
+                float positionY_texte = alliee[j].PositionY+40; // Exemple de position Y (vous pouvez ajuster selon vos besoins)
                 // Debug.Log(positionX_texte);
 
-                liste_texte_pv[i].MettreAJourTextePV(unite2.Pv,positionX_texte,positionY_texte,0);
-            }
-            
-            // Mettre à jour la position des unités
-            if(unites[0] != null && unites[1] != null)
+                //liste_texte_pv[i].MettreAJourTextePV(unite2.Pv,positionX_texte,positionY_texte,0);
+
+                // Mettre à jour la position des unités
+                if(unites[0] != null && unites[1] != null)
+                {
+                    unites[0].transform.position = new Vector3(alliee[j].PositionX, alliee[j].PositionY, alliee[j].PositionZ);
+                    unites[1].transform.position = new Vector3(ennemie[0].PositionX, ennemie[0].PositionY, ennemie[0].PositionZ);
+
+                    if(ennemie[0].Pv <= 0)
+                    {
+                        Debug.Log("L'unite 2 est morte");
+
+                        Destroy(unites[1]);
+                        unites.RemoveAt(1);
+                        animator.SetBool("Won",true);
+                    }
+                    else
+                    {
+                        alliee[j].Attaquer(ennemie[0]);
+                        //Debug.Log("PV de unite2 après attaque : " + unite2.Pv);
+                    }
+                }
+            }               
+            for(int t=0;t<nb_ennemie;t++)//Parcours du nombre d'alliées
             {
-                unites[0].transform.position = new Vector3(unite1.PositionX, unite1.PositionY, unite1.PositionZ);
-                unites[1].transform.position = new Vector3(unite2.PositionX, unite2.PositionY, unite1.PositionZ);
-
-                if(unite2.Pv <= 0)
-                {
-                    Debug.Log("L'unite 2 est morte");
-
-                    Destroy(unites[1]);
-                    unites.RemoveAt(1);
-                    animator.SetBool("Won",true);
-                }
-                else
-                {
-                    unite1.Attaquer(unite2);
-                    //Debug.Log("PV de unite2 après attaque : " + unite2.Pv);
-                }
-            }
+                ennemie[t].DeplacerVersUniteDifferente(alliee[0], canva_pour_texte_pv);
+            
+            }               
+            
         }
+            
 
     }
 
